61c96eb79b7af6da441fefbfc832d0b7b56f129b
celia albano <albano.celia@gmail.com>
2017-05-05 19:33:17 +0200

    prepare for heroku

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,6 +18,30 @@ class ProjectsController < ApplicationController
     @projects = Project.where(name: 'soretec')
   end
 
+  def archil
+    @projects = Project.where(name: 'archil')
+  end
+
+  def dmum
+    @projects = Project.where(name: 'dmum')
+  end
+
+  def ose
+    @projects = Project.where(name: 'ose')
+  end
+
+  def fermepc
+    @projects = Project.where(name: 'fermepc')
+  end
+
+  def equiperle
+    @projects = Project.where(name: 'equiperle')
+  end
+
+  def pelemele
+    @projects = Project.where(name: 'pelemele')
+  end
+
   def set_list
     @db_constants = YAML.load_file(Rails.root.join('config', 'constants.yml'))
     @list = @db_constants['projects'].keys
