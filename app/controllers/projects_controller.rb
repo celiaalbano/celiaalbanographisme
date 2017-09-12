f7f475515fe6a8cc961dac1d473a02cce02c7ad3
celia albano <albano.celia@gmail.com>
2017-09-12 12:51:06 +0200

    test controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,49 +1,49 @@
-class ProjectsController < ApplicationController
+# class ProjectsController < ApplicationController
 
-  before_action :set_list, only: [:show]
+#   before_action :set_list, only: [:show]
 
-  def show
-  end
+#   def show
+#   end
 
-  def index
-  end
+#   def index
+#   end
 
-  def new
-  end
+#   def new
+#   end
 
-  def edit
-  end
+#   def edit
+#   end
 
-  def soretec
-    @projects = Project.where(name: 'soretec')
-  end
+#   def soretec
+#     @projects = Project.where(name: 'soretec')
+#   end
 
-  def archil
-    @projects = Project.where(name: 'archil')
-  end
+#   def archil
+#     @projects = Project.where(name: 'archil')
+#   end
 
-  def dmum
-    @projects = Project.where(name: 'dmum')
-  end
+#   def dmum
+#     @projects = Project.where(name: 'dmum')
+#   end
 
-  def ose
-    @projects = Project.where(name: 'ose')
-  end
+#   def ose
+#     @projects = Project.where(name: 'ose')
+#   end
 
-  def fermepc
-    @projects = Project.where(name: 'fermepc')
-  end
+#   def fermepc
+#     @projects = Project.where(name: 'fermepc')
+#   end
 
-  def equiperle
-    @projects = Project.where(name: 'equiperle')
-  end
+#   def equiperle
+#     @projects = Project.where(name: 'equiperle')
+#   end
 
-  def pelemele
-    @projects = Project.where(name: 'pelemele')
-  end
+#   def pelemele
+#     @projects = Project.where(name: 'pelemele')
+#   end
 
-  def set_list
-    @db_constants = YAML.load_file(Rails.root.join('config', 'constants.yml'))
-    @list = @db_constants['projects'].keys
-  end
-end
+#   def set_list
+#     @db_constants = YAML.load_file(Rails.root.join('config', 'constants.yml'))
+#     @list = @db_constants['projects'].keys
+#   end
+# end
